483cc3c61242bfd5a35326edd44b7dd3b64520ba
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-17 20:22:36 +0000

    initial setup for the MessageBar component

---
+++ src/MessageBar.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+//component for displaying and receiving new username and receiving a new message content
+class MessageBar extends Component {
+
+  render() {
+
+    return (
+      //display of chatbar components, username and input message
+      // <footer className="chatbar">
+      <div className="columns">
+        <div className="column">
+          <div class="field">
+            <div class="control">
+              <span>BOB</span>
+            </div>
+          </div>
+        </div>
+        <div className="column is-three-quarters">
+          <div class="field">
+            <div class="control">
+              <input className="input is-primary" name="inputMessage" type="text"
+                  placeholder="Type a message and hit ENTER"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default MessageBar;
