ee43424c68421a807818fad1f99935b8b7dcf603
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-17 20:21:12 +0000

    setting up the message component

---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+
+  render() {
+
+    return (
+      // display the message
+      <div>
+        Message
+      </div>
+    );
+
+  }
+}
+
+export default Message;
