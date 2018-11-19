1cc9e02278782e2e69c42cf3bfc591c27b8235bf
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-19 02:41:49 +0000

    styled elements on the chat main page

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* create navbar component which includes a logo and a span element for displaying the number of users */}
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">WEA or NAY</a>
-          <span className="navbar-users">Viewer Avatar</span>
+
+        {/* create a nav component which includes a logo and a span element for displaying the viewer avatar and profile */}
+        <nav className="level">
+          <div claName="level-left">
+            <div className="level-item has-text-centered">
+              <p id="logo">YEA or NAY</p>
+            </div>
+          </div>
+          <div claName="level-left">
+            <div className="level-item">
+              <span className="navbar-users">Viewer</span>
+            </div>
+          </div>
         </nav>
 
         <div className="section">
@@ -21,25 +30,25 @@ class App extends Component {
             <div className="columns">
 
               <div className="column">
+               <h5 className="subtitle is-5">Propose Debate:</h5>
                 <div className="box">
                   <div className="field">
-                    <h4 className="subtitle is-4">Propose Debate:</h4>
                     <div className="control">
                       <input className="input is-primary" type="text" placeholder="Debate input"/>
                       <div className="buttons has-addons">
-                        <span className="button">WEA</span>
+                        <span className="button">YEA</span>
                         <span className="button">NAY</span>
                       </div>
                     </div>
                   </div>
                 </div>
-
+                  <h5 className="subtitle is-5">Join Debate:</h5>
                   <div className="box">
                     <div className="field">
-                      <h4 className="subtitle is-4">Join A Debate:</h4>
                       <div className="control">
                         <ul>
-                          <li>This is our very long debate title<input class="button" type="submit" value="Wea or Nay"/></li>
+                          <li>This is our very long debate title</li>
+                          <li><input class="button" type="submit" value="Yea or Nay"/></li>
                         </ul>
                       </div>
                     </div>
