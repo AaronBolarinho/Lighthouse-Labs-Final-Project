f6a25bfe3e53ab10c518aad9cafac75bdd079b3f
kevinmacarthur <kmacarthur16@gmail.com>
2018-11-19 21:25:52 +0000

    added a proposed debate list item and the proposed debate list component to render all of its debates

--- Yea_or_nay/src/ProposedDebateList.jsx
+++ Yea_or_nay/src/ProposedDebateList.jsx
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
+import ProposedDebateListItem from './ProposedDebateListItem.jsx'
 
 class ProposedDebateList extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      topics: [{id: 1, proposingUser: "TestUser1", topicStatement: "Aardvarks are superior to narwhales", stance:"Yea"}, {id: 2, proposingUser: "TestUser2", topicStatement: "Avocados are good for exfoliation", stance:"Nay"}],
+    }
+  }
+
+  createTopics () {
+    const topicList=this.state.topics.map(topic => {
+      return(
+        <ProposedDebateListItem key={topic.id} topic={topic.topicStatement} proposingUser={topic.proposingUser} stance={topic.stance} />
+      )
+    })
+    return topicList
+  }
 
   render() {
 
@@ -9,7 +27,7 @@ class ProposedDebateList extends Component {
                     <div className="field">
                       <div className="control">
                         <ul>
-                          <li>This is our very long debate title<input className="button" type="submit" value="Yea or Nay"/></li>
+                          {this.createTopics()}
                         </ul>
                       </div>
                     </div>
---
+++ Yea_or_nay/src/ProposedDebateListItem.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class ProposedDebateListItem extends Component {
+   constructor(props) {
+    super();
+    this.checkSupportStatus = this.checkSupportStatus.bind(this)
+  }
+  checkSupportStatus () {
+    let supports = "Nay"
+    if (this.props.stance === "Nay") {
+      supports = "Yea"
+    }
+    return supports
+  }
+  render() {
+    return (
+            <li>{this.props.proposingUser} proposes: {this.props.topic}<input className="button" type="submit" value={this.checkSupportStatus()}/></li>
+    );
+  }
+}
+
+export default ProposedDebateListItem;
