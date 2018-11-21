b0e7824b70fd098f38b83f907f7e4f8b1ae9ab07
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-21 16:05:37 +0000

    cleaned up code

--- Yea_or_nay/src/Home.jsx
+++ Yea_or_nay/src/Home.jsx
@@ -1,16 +1,10 @@
 import React, {Component} from 'react';
-import MessageList from './MessageList.jsx';
-import MessageBar from './MessageBar.jsx';
 import DebateRoom from './DebateRoom.jsx';
-import Message from './Message.jsx';
 import ProposedDebate from './ProposedDebate.jsx';
 import ProposedDebateList from './ProposedDebateList.jsx';
-// import ProposedDebate from '.ProposedDebate.jsx';
-// import ProposedDebateList from '.ProposedDebateList.jsx';
-// import ActiveDebateList from '.ActiveDebateList.jsx';
-import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
-const io = require('socket.io-client')
-const socket = io.connect('http://localhost:3001')
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3001');
 
 class Home extends Component {
   constructor(props) {
@@ -21,7 +15,7 @@ class Home extends Component {
     }
   }
 
-renderDebateRoom(debateRoom) {
+  renderDebateRoom(debateRoom) {
     return (
       <DebateRoom debateRoom={debateRoom}/>
     )
@@ -29,43 +23,36 @@ renderDebateRoom(debateRoom) {
 
   render() {
     return (
-      <div>
 
-        <div className="section">
-          <div className="container">
-            <div className="columns">
+      <div className='container-fluid'>
+        <div className='row'>
 
-              <div className="column">
-                <h5 className="subtitle is-5">Propose Debate:</h5>
-                <ProposedDebate/>
-                <h5 className="subtitle is-5">Join Debate:</h5>
-                <ProposedDebateList/>
-
-
-              </div>
-               <div className="column is-three-quarters">
-                <div className="field">
-                  <div className="control">
-                    {/* include the MessageList component to display the messages sent and their sender names */}
-                    <DebateRoom debateRoom={{name:"mainroom"}}/>
-                  </div>
-                </div>
-                  <li>
-                  <Link to ="/Room1">Room1</Link>
-                  </li>
-                  <li>
-                  <Link to ="/Room2">Room2</Link>
-                  </li>
-                  <li>
-                  <Link to ="/Room3">Room3</Link>
-                  </li>
-                  </div>
-
-            </div>
+          <div className='col-sm-5'>
+            <h5 className="subtitle is-5">Propose Debate:</h5>
+            <ProposedDebate/>
+            <h5 className="subtitle is-5">Join Debate:</h5>
+            <ProposedDebateList/>
           </div>
+
+          <div className='col-sm-7'>
+            {/* include the MessageList component to display the messages sent and their sender names */}
+            <DebateRoom debateRoom={{name:"mainroom"}}/>
+
+            <li>
+            <Link to ="/Room1">Room1</Link>
+            </li>
+            <li>
+            <Link to ="/Room2">Room2</Link>
+            </li>
+            <li>
+            <Link to ="/Room3">Room3</Link>
+            </li>
+          </div>
+
         </div>
       </div>
     );
   }
 }
+
 export default Home;
