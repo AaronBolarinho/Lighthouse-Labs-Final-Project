93a3cae01d86baba2ff7c5229e4dc576b176f3c4
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-23 19:36:38 +0000

    modified code to accept progress value passed down from the DebateMessageList component

---
+++ Yea_or_nay/src/ProgressBar.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class ProgressBar extends Component {
+
+  constructor(props) {
+    super();
+    // this.state = {
+    //   value: 50
+    // };
+
+    // this.updateValue=this.updateValue.bind(this);
+  }
+
+  // updateValue() {
+  //   this.setState({value: this.props.value});
+  // }
+
+  // componentDidMount() {
+  //   this.setState({value: this.props.value});
+  // }
+
+  render() {
+    console.log('this.props.value', this.props.value);
+    return (
+      <div className="progress-bar-container">
+        <input type="range" min="0" max="100" value={this.props.value} className="progress-bar" id="grad" step="1"/>
+      </div>
+    )
+  }
+}
+
+export default ProgressBar;
