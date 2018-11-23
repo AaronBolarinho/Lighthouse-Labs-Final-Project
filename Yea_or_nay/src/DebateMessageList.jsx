0baa8259d75ef8e8ce8c86c733c2410f93c54830
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-23 19:39:33 +0000

    added debator1Liked, debator2Liked parameters to debateMessageList function which we used to calculate progress_value for the progress bar used in the DebateMessageList component

--- Yea_or_nay/src/DebateMessageList.jsx
+++ Yea_or_nay/src/DebateMessageList.jsx
@@ -1,21 +1,28 @@
 import React, {Component} from 'react';
 import DebateRoomMessage from './DebateRoomMessage.jsx';
+import ProgressBar from './ProgressBar.jsx';
 
-function DebateMessageList ({messages, debateRoom,updateLiked, userState}) {
+function DebateMessageList ({messages, debateRoom,updateLiked, userState, debator1Liked, debator2Liked}) {
+  console.log('debator1Liked', debator1Liked);
+  console.log('debator1Liked', debator2Liked);
+  let progress_value = 100 * debator2Liked / (debator1Liked + debator2Liked);
+  console.log('progress_value', progress_value);
   const messageList = messages.map(message => {
     return(
       <DebateRoomMessage key={message.id} message={message.content} username={message.username} room={debateRoom.name}  updateLiked={updateLiked} state={userState} flag={message.flag}/>
     )
-  })
+  });
   return (
         <div className="messages">
-          <h4 className='room-name'>{debateRoom.name}</h4>
           <h4 className='room-name'>{debateRoom.proposedDebate} </h4>
+          {debateRoom.name !== 'mainroom' ?  <ProgressBar value={progress_value}/> : ""}
+          <h4 className='debator-name1'>{debateRoom.debator1}</h4>
+          <h4 className='debator-name2'>Debator 2</h4>
           <div className='container message-list'>
-              {messageList}
+            {messageList}
           </div>
         </div>
-  )
+  );
 }
 
 export default DebateMessageList
