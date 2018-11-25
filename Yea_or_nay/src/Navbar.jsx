836d640bf48665488921393255f15edeaef2afa6
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-25 19:57:31 +0000

    moved Navbar elements in the navbar component and added Navbar component to App.jsx

--- Yea_or_nay/src/Navbar.jsx
+++ Yea_or_nay/src/Navbar.jsx
@@ -1,26 +1,55 @@
 import React, {Component} from 'react';
+import TestChangeUser from './TestChangeUser.jsx';
 
-class Navbar extends Component {
+function Navbar ({changeUsername, currentUser}) {
 
-  render() {
+  if (currentUser.name === 'bob') {
     return (
       <nav className='navbar navbar-expand-sm navbar-black'>
         <div className='container'>
           <div className="navbar-brand">
             <p id="logo">YEA or NAY</p>
           </div>
-
-          <TestChangeUser changeUsername={this.changeUsername}/>
-
           <div className="navbar-nav navbar-right">
             <div className="nav-item">
-              <span className="navbar-users">Viewer</span>
+              <span className="navbar-users"><TestChangeUser changeUsername={changeUsername}/></span>
             </div>
           </div>
         </div>
       </nav>
     )
-  }
+  } else {
+    return(
+       <nav className='navbar navbar-expand-sm navbar-black'>
+      <div className='container'>
+        <div className="navbar-brand">
+          <p id="logo">YEA or NAY</p>
+        </div>
+        <div className="navbar-nav navbar-right">
+          <div className="nav-item">
+            <span className="navbar-users">{currentUser.name}</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )};
+
+
+  {/*return (
+    <nav className='navbar navbar-expand-sm navbar-black'>
+      <div className='container'>
+        <div className="navbar-brand">
+          <p id="logo">YEA or NAY</p>
+        </div>
+          {currentUser.name === 'bob' ? <TestChangeUser changeUsername={changeUsername}/> : {currentUser.name}}
+        <div className="navbar-nav navbar-right">
+          <div className="nav-item">
+            <span className="navbar-users">Viewer</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )*/}
 }
 
 export default Navbar;
