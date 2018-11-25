536d072d69c0ba1e037a5d716fadcf2682aa8250
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-25 14:45:30 +0000

    added divs and classes to individual messages to show them in a bspeech bubble

--- Yea_or_nay/src/DebateRoomMessage.jsx
+++ Yea_or_nay/src/DebateRoomMessage.jsx
@@ -11,9 +11,13 @@ function DebateRoomMessage ({username, message, room, updateLiked, state, flag})
    }
 
   return (
-      <div className="message" >
-        <span className="message-username">{username}: </span>
-          <span className="message-content">{message} {room !== 'mainroom' && state === 'viewer' &&  !flag ? <button id='liked' onClick={clickedLike}>LIKE</button>: ''}</span>
+      <div>
+        <div>
+          <span className="message-username">{username}: </span>
+        </div>
+        <div className="message">
+          <div className="message-content speech-bubble top">{message} {room !== 'mainroom' && state === 'viewer' &&  !flag ? <button id='liked' onClick={clickedLike}>LIKE</button>: ''}</div>
+        </div>
       </div>
   );
 }
